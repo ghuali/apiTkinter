29ecc54d81b1ab14680e4d2d1d3b92e1cf659257
ghuali <guaghuali@gmail.com>
2024-10-24 12:55:49 +0100

    añadido botones debajo de cada label

--- vistas.py
+++ vistas.py
@@ -37,9 +37,9 @@ def mostrar_productos(productos_data):
     precio = canvas.create_text(50, 175, anchor="nw", font=('inter', 15))
     rating = canvas.create_text(50, 225, anchor="nw", font=('inter', 15))
     warranty = canvas.create_text(50, 275, anchor="nw", font=('inter', 15))
-    stock = canvas.create_text(50, 325, anchor="nw", font=('inter', 15))
+    stock = canvas.create_text(400, 340, anchor="center", font=('inter', 15))
     compra_llega = canvas.create_text(740, 275, anchor="ne", font=('inter', 15))
-    sku = canvas.create_text(400, 500, anchor="center", font=('inter', 15))
+    sku = canvas.create_text(400, 290, anchor="center", font=('inter', 15))
 
     boton_siguiente = ttk.Button(root, text="Siguiente", command=siguiente)
     boton_siguiente.place(x=600, y=500, anchor="center", width=150)
@@ -74,9 +74,9 @@ def mostrar_informacion_producto():
     canvas.itemconfig(categoria, text=producto.category)
     canvas.itemconfig(marca, text=producto.brand)
     canvas.itemconfig(precio, text=f"{producto.price}€")
-    canvas.itemconfig(rating, text=producto.rating)
+    canvas.itemconfig(rating, text=f"{producto.rating}★")
     canvas.itemconfig(warranty, text=producto.warranty_information)
-    canvas.itemconfig(stock, text=producto.stock)
+    canvas.itemconfig(stock, text=producto.availability_status)
     canvas.itemconfig(compra_llega, text=producto.shipping_information)
     canvas.itemconfig(sku, text=producto.sku)
 
@@ -103,14 +103,17 @@ def buscar_producto():
 
 def mostrar_listado():
     listado_ventana = tk.Tk()
+    listado_ventana.title("busqueda intensa brrrrr")
 
     global indice
     buscar = search_entry.get().lower()
 
     for producto in data.products:
         if buscar in producto.title.lower():
-            label =ttk.Label(text=producto.title)
+            label = ttk.Label(listado_ventana,text=producto.title)
             label.pack()
+            boton = ttk.Button(listado_ventana,text="seleccionar",command=buscar_producto)
+            boton.pack()
 
 
     listado_ventana.mainloop()
